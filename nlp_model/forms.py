33e1940f130616c85f04bdeed512349f354d3003
Manuel Ramirez Garcia <mramire6@users.noreply.github.com>
2020-06-15 19:55:39 -0500

    created form for user to select news section and date; first pass at how routes will be organized including examples that are commented out

--- nlp_model/forms.py
+++ nlp_model/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField, StringField
+from wtforms import SelectField, SubmitField, StringField, DateField
 from wtforms.validators import DataRequired, Length
 from config import Defaults
 
 
 class UserSelectionForm(FlaskForm):
-    section = SelectField('Section',
-                          choices=Defaults.section_default)
-    topic = StringField('Topic', validators=[DataRequired(), Length(min=2, max=20)])
+    latest = SubmitField('Latest')
+    section = SelectField('Section',choices=Defaults.section_default)
+    date = DateField('Date', format='%Y-%m-%d')
+    #topic = StringField('Topic', validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField('Submit')
+
+class GetLatestForm(FlaskForm):
+    latest = SubmitField('Get Latest Articles')
 
 #This is where the forms for the user will live. The user will input what the
 #section
