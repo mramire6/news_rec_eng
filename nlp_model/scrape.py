f297de21dd4529df64ee72d2497dd84c97718461
Manuel Ramirez Garcia <mramire6@users.noreply.github.com>
2020-06-15 19:50:27 -0500

    Changed Scrape class attributes to dict to be cleaner; made changes to unit tests to match; added three functions as pipelines that chain methods together

--- nlp_model/scrape.py
+++ nlp_model/scrape.py
@@ -1,63 +1,99 @@
 import requests
 from bs4 import BeautifulSoup
-
-url1 = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c-4d5d-b4b7-891113b56233.html"
-url2 = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c.html"
-url3 = "https://www.axios.com/politics-policy/election"
+import time
 
 class Scrape():
-    def __init__(self, url):
-        self.url = url
-        self.ro = None #checked
-        self.page = None #checked
-        self.code = None #checked
-        self.soup = None #checked
-        self.article = None
-        self.main_text = None
-        self.title = None
-        self.authors = None
-        self.section = None
-        self.word_count = None
+    def __init__(self, url = None, soup = None):
+        self.soup_ing = {'url': url,
+                         'ro': None,
+                         'page': None,
+                         'soup': soup}
+        self.main_info = {'article': None,
+                          'main_text': None,
+                          'headline': None,
+                          'authors': None,
+                          'section': None,
+                          'word_count': None}
 
     def get_request_object(self):
-        self.ro = requests.get(self.url)
+        # As per Axios' editors permission maximum of 10 requests per 60 seconds.
+        time.sleep(6)
+        self.soup_ing['ro'] = requests.get(self.soup_ing['url'])
 
     def get_web_page(self):
-        self.page = self.ro.content
+        self.soup_ing['page'] = self.soup_ing['ro'].content
 
     def check_status_code(self):
-        code = self.ro.status_code
+        code = self.soup_ing['ro'].status_code
         if code != 200:
             raise Exception('Web page was not found')
         else:
-            self.code = code
+            self.soup_ing['code'] = code
 
     def soupify_webpage(self):
-        self.soup = BeautifulSoup(self.page, 'html.parser')
+        self.soup_ing['soup'] = BeautifulSoup(self.soup_ing['page'], 'html.parser')
 
     def isolate_article(self):
-        temp = self.soup.find('article')
+        temp = self.soup_ing['soup'].find('article')
         if temp is None:
             raise Exception('No article was found')
         else:
-            self.article = temp
+            self.main_info['article'] = temp
 
     def isolate_text(self, class_type = None):
         if class_type is None:
             class_type = 'b0w77w-0 jctzOA gtm-story-text p mt-12 mb-20 sm:mt-20'
         else:
             class_type = class_type
-        self.main_text = self.article.find('div', class_=class_type).text
+        self.main_info['main_text'] = self.main_info['article'].find('div', class_=class_type).text
 
     def isolate_headline(self):
-        self.title = self.soup.title.text
+        self.main_info['headline'] = self.soup_ing['soup'].title.text
 
     def isolate_authors(self):
-        authors_html = self.article.find('div', class_='truncate').find_all('a')
-        self.authors = [authors_html[ind].text for ind in range(len(authors_html))]
+        authors_html = self.main_info['article'].find('div', class_='truncate').find_all('a')
+        self.main_info['authors'] = [authors_html[ind].text for ind in range(len(authors_html))]
 
     def isolate_section(self):
-        self.section = self.article.find('div').a['href']
+        self.main_info['section'] = self.main_info['article'].find('div').a['href']
 
     def find_word_count(self):
-        self.word_count = len(self.main_text.split())
+        self.main_info['word_count'] = len(self.main_info['main_text'].split())
+
+    def get_main_info(self):
+        return self.main_info
+
+    ###########
+
+    def get_soup(self):
+        self.get_request_object()
+        self.check_status_code()
+        self.get_web_page()
+        self.soupify_webpage()
+        return self.soup_ing
+
+    def get_info_articles(self):
+        self.isolate_article()
+        self.isolate_text()
+        self.isolate_headline()
+        self.isolate_authors()
+        self.section()
+        self.find_word_count()
+        return self.main_info
+
+    def pipeline(self):
+        self.get_request_object()
+        self.check_status_code()
+        self.get_web_page()
+        self.soupify_webpage()
+        self.isolate_article()
+        self.isolate_text()
+        self.isolate_headline()
+        self.isolate_authors()
+        self.section()
+        self.find_word_count()
+        return self.main_info
+
+
+
+
--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,12 +1,17 @@
 from nlp_model.scrape import Scrape
 import pytest
 
+###### Dummy test data for test_scrape.py #######
+
+dummy_working_url = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c-4d5d-b4b7-891113b56233.html"
+dummy_not_working_url = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c.html"
+dummy_working_incorrect_url = "https://www.axios.com/politics-policy/election"
+
 #####################
 
 @pytest.fixture(scope='module')
 def scrape_correct_url():
-    url = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c-4d5d-b4b7-891113b56233.html"
-    s = Scrape(url)
+    s = Scrape(dummy_working_url)
     s.get_request_object()
     s.check_status_code()
     s.get_web_page()
@@ -21,15 +26,13 @@ def scrape_correct_url():
 
 @pytest.fixture(scope='module')
 def scrape_404_url():
-    url = "https://www.axios.com/facebook-employees-stage-virtual-walkout-7f28483e-d59c.html"
-    s = Scrape(url)
+    s = Scrape(dummy_not_working_url)
     s.get_request_object()
     return s
 
 @pytest.fixture(scope='module')
 def scrape_incorrect_working_url():
-    url = "https://www.axios.com/politics-policy/election"
-    s = Scrape(url)
+    s = Scrape(dummy_working_incorrect_url)
     s.get_request_object()
     s.check_status_code()
     s.get_web_page()
@@ -40,20 +43,20 @@ def scrape_incorrect_working_url():
 
 # Test whether request object was made correctly
 def test_constructor1(scrape_correct_url):
-    assert scrape_correct_url.ro is not None
+    assert scrape_correct_url.soup_ing['ro'] is not None
 
 def test_constructor2(scrape_404_url):
-    assert scrape_404_url.ro is not None
+    assert scrape_404_url.soup_ing['ro'] is not None
 
 def test_constructor3(scrape_incorrect_working_url):
-    assert scrape_incorrect_working_url.ro is not None
+    assert scrape_incorrect_working_url.soup_ing['ro'] is not None
 
 # Test whether status code was correctly identified or exception raised
 def test_web_page_found1(scrape_correct_url):
-    assert scrape_correct_url.code == 200
+    assert scrape_correct_url.soup_ing['code'] == 200
 
 def test_web_page_found1(scrape_incorrect_working_url):
-    assert scrape_incorrect_working_url.code == 200
+    assert scrape_incorrect_working_url.soup_ing['code'] == 200
 
 def test_web_page_found1(scrape_404_url):
     with pytest.raises(Exception):
@@ -61,14 +64,14 @@ def test_web_page_found1(scrape_404_url):
 
 # Test whether a soup was made
 def test_soupify_webpage1(scrape_correct_url):
-    assert scrape_correct_url.soup is not None
+    assert scrape_correct_url.soup_ing['soup'] is not None
 
 def test_soupify_webpage2(scrape_incorrect_working_url):
-    assert scrape_incorrect_working_url.soup is not None
+    assert scrape_incorrect_working_url.soup_ing['soup'] is not None
 
 # Test whether an article was found or not
 def test_isolate_article1(scrape_correct_url):
-    assert scrape_correct_url.article is not None
+    assert scrape_correct_url.main_info['article'] is not None
 
 def test_isolate_article2(scrape_incorrect_working_url):
     with pytest.raises(Exception):
@@ -76,21 +79,21 @@ def test_isolate_article2(scrape_incorrect_working_url):
 
 # Test whether data was able to be extracted from article
 def test_isolate_text(scrape_correct_url):
-    assert scrape_correct_url.main_text is not None
+    assert scrape_correct_url.main_info['main_text'] is not None
 
 def test_isolate_main_headline(scrape_correct_url):
-    assert scrape_correct_url.title is not None
-    assert isinstance(scrape_correct_url.title, str)
+    assert scrape_correct_url.main_info['headline'] is not None
+    assert isinstance(scrape_correct_url.main_info['headline'], str)
 
 def test_isolate_main_authors(scrape_correct_url):
-    assert len(scrape_correct_url.authors) >= 1
-    assert isinstance(scrape_correct_url.authors[0], str)
+    assert len(scrape_correct_url.main_info['authors']) >= 1
+    assert isinstance(scrape_correct_url.main_info['authors'][0], str)
 
 def test_isolate_section(scrape_correct_url):
-    assert scrape_correct_url.section is not None
-    assert isinstance(scrape_correct_url.section, str)
+    assert scrape_correct_url.main_info['section'] is not None
+    assert isinstance(scrape_correct_url.main_info['section'], str)
 
 def test_find_word_count(scrape_correct_url):
-    assert scrape_correct_url.word_count is not None
-    assert scrape_correct_url.word_count > 0
-    assert isinstance(scrape_correct_url.word_count, int)
+    assert scrape_correct_url.main_info['word_count'] is not None
+    assert scrape_correct_url.main_info['word_count'] > 0
+    assert isinstance(scrape_correct_url.main_info['word_count'], int)
